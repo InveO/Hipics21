f02ce1b89a53dcc8a5cb082387d09173193348b2
daniel <drobinson@inveo.be>
2012-04-05 16:41:55 +0200

    init release

---
+++ Hipics21/javasource/jet/hipics21/ui/desktop/Hipics21MenuConstants.java
@@ -0,0 +1,17 @@
+package jet.hipics21.ui.desktop;
+
+/**
+ * @author daniel
+ * 
+ */
+public interface Hipics21MenuConstants {
+    /**
+     * <code>MAIN_MENU_BAR</code>
+     */
+    public static final String MAIN_MENU_BAR = "Hipics21MainMenuBar";
+    /**
+     * <code>MAIN_MENU_BAR</code>
+     */
+    public static final String GROUP_MAIN_MENU = "mainMenuBarGroup";
+
+}
